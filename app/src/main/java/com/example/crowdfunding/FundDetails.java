0523262448464ba5e0e557a6f328e6544e497c96
shreyasshettyb <shreyasb52@gmail.com>
2023-06-14 05:47:30 +0530

    XML code done

--- app/src/main/java/com/example/crowdfunding/FundDetails.java
+++ app/src/main/java/com/example/crowdfunding/FundDetails.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class VolunteerHome extends AppCompatActivity {
+public class FundDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home);
+        setContentView(R.layout.activity_fund_details);
     }
 }
--- app/src/main/java/com/example/crowdfunding/TotalCollection.java
+++ app/src/main/java/com/example/crowdfunding/TotalCollection.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class VolunteerHome extends AppCompatActivity {
+public class TotalCollection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin_home);
+        setContentView(R.layout.activity_total_collection);
     }
 }
