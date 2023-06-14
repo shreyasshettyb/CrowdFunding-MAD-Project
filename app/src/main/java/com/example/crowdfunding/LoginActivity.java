576634aec65a6cc6831ae8021425b58ba0cf3e90
Sourabh <sourabhp94503@gmail.com>
2023-06-14 23:43:03 +0530

    Added java codes for signup and login

--- app/src/main/java/com/example/crowdfunding/LoginActivity.java
+++ app/src/main/java/com/example/crowdfunding/LoginActivity.java
@@ -4,21 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.crowdfunding.db.DBHelper;
 
 public class LoginActivity extends AppCompatActivity {
-    TextView crt;
+    EditText username;
+    EditText password;
+    TextView signup;
+    DBHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-
-        crt=(TextView) findViewById(R.id.crt_acc);
-        crt.setOnClickListener(v -> {
+        setContentView(R.layout.activity_login);
+        username = findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        signup = findViewById(R.id.linkToSignUp);
+        helper = new DBHelper(this, "infodb", null, 1);
+        signup.setOnClickListener(v -> {
                 Intent i = new Intent(LoginActivity.this,SignupActivity.class);
                 startActivity(i);
             }
         );
+    }
 
+    public void onLogin(View v){
+        if( helper.checkUser(username.getText().toString(), password.getText().toString()) ){
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(LoginActivity.this, AdminHome.class);
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "Entered Credentials do not match", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/example/crowdfunding/SignupActivity.java
+++ app/src/main/java/com/example/crowdfunding/SignupActivity.java
@@ -2,13 +2,83 @@ package com.example.crowdfunding;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.crowdfunding.db.DBHelper;
+import com.example.crowdfunding.db.User;
+
+import java.util.regex.Pattern;
 
 public class SignupActivity extends AppCompatActivity {
-
+    EditText name;
+    EditText email;
+    EditText password;
+    EditText conPassword;
+    Spinner type;
+    EditText fundingCode;
+    DBHelper helper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.signupPassword);
+        conPassword = findViewById(R.id.conPassword);
+        fundingCode = findViewById(R.id.fundingCode);
+        type = findViewById(R.id.type);
+        helper = new DBHelper(this, "infodb", null, 1);
+    }
+
+    public void signUp(View v){
+        if( isPasswordValid()) {
+            if(helper.isUserPresent(email.getText().toString())){
+                Toast.makeText(this, "Entered Credentials already exists", Toast.LENGTH_LONG).show();
+            }
+            else {
+                if (type.getSelectedItem().toString().equals("Admin")) {
+                    fundingCode.setText("");
+                }
+                if (helper.addUser(new User(name.getText().toString(), email.getText().toString(), password.getText().toString(), type.getSelectedItem().toString(), fundingCode.getText().toString()))){
+                    Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(SignupActivity.this, AdminHome.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(this, "Error occurred during registration, Please try again", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    private boolean isPasswordValid(){
+        String msg;
+        Pattern lowerCase = Pattern.compile("^.*[a-z].*$");
+        Pattern upperCase = Pattern.compile("^.*[A-Z].*$");
+        Pattern number = Pattern.compile("^.*[0-9].*$");
+        //Pattern specialCharacter = Pattern.compile("^.*[^a-zA-Z0-9].*$");
+        String pass = password.getText().toString(), repass = conPassword.getText().toString();
+        if(pass.equals("") || repass.equals(""))
+            msg = "Enter password and Confirm it";
+        else if(pass.length() < 8)
+            msg = "Password must at least be 8 characters long";
+        else if(! lowerCase.matcher(pass).matches())
+            msg = "Password must at least have one lowercase letter";
+        else if(! upperCase.matcher(pass).matches())
+            msg = "Password must at least have one uppercase letter";
+        else if(! number.matcher(pass).matches())
+            msg = "Password must at least have one digit";
+        else if(! pass.equals(repass))
+            msg = "Both passwords must be same";
+        else
+            return true;
+
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+        return false;
     }
 }
